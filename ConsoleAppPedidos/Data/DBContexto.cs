a6ec37ac3508c7179fc39b24cf5e77fda005fe8b
Robson Paivaq <paivawork@hotmail.com>
2023-07-07 19:20:08 -0300

    Adicionada documentação da Classe DBContexto

--- ConsoleAppPedidos/Data/DBContexto.cs
+++ ConsoleAppPedidos/Data/DBContexto.cs
@@ -3,15 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ConsoleAppPedidos.Data
 {
+    /// <summary>
+    /// Classe que configura o Banco de Dados
+    /// </summary>
     public class DBContexto : DbContext
     {
+        /// <summary>
+        /// Método que configura a conexão com o Banco de Dados SQL Server.
+        /// </summary>
+        /// <param name="optionsBuilder">Parâmetro para configuração do BD.</param>
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //String de conexão com Banco de Dados SQL Server
             optionsBuilder.UseSqlServer("Server=localhost;Database=DBConsoleAppPedidos;User ID=SA;Password=Password123;Encrypt=false;TrustServerCertificate=false;Connection Timeout=30;");
         }
 
+        /// <summary>
+        /// Propriedade que representará a tabela Produtos do BD.
+        /// </summary>
         public DbSet<Produto> Produto { get; set; }
+        /// <summary>
+        /// Propriedade que representará a tabela Pedidos do BD.
+        /// </summary>
         public DbSet<Pedido> Pedido { get; set; }
+        /// <summary>
+        /// Propriedade que representará a tabela ItensDePedido do BD.
+        /// </summary>
         public DbSet<ItensDePedido> ItensDePedido { get; set; }
     }
 }

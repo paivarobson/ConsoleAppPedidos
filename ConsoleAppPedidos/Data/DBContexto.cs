9eea06d9e061034404c3677cd09f19ed4dc9437d
Robson Paivaq <paivawork@hotmail.com>
2023-07-07 14:55:00 -0300

    Adicionado controle de dados

---
+++ ConsoleAppPedidos/Data/DBContexto.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleAppPedidos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleAppPedidos.Data
+{
+    public class DBContexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DBConsoleAppPedidos;User ID=SA;Password=Password123;Encrypt=false;TrustServerCertificate=false;Connection Timeout=30;");
+        }
+
+        public DbSet<Pedido> Pedido { get; set; }
+    }
+}

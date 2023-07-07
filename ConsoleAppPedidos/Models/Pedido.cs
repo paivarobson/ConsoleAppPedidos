93e2d9cf9b6d3ca69b7d83f4be364679530b58a6
Robson Paivaq <paivawork@hotmail.com>
2023-07-07 19:08:37 -0300

    Adicionado DataAnnotations nas entidades

--- ConsoleAppPedidos/Models/Pedido.cs
+++ ConsoleAppPedidos/Models/Pedido.cs
@@ -1,25 +1,37 @@
-﻿namespace ConsoleAppPedidos.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppPedidos.Models
 {
     /// <summary>
     /// Classe Pedido.
     /// </summary>
+    [Table("Pedidos")]
     public class Pedido
     {
         /// <summary>
         /// ID do Pedido.
         /// </summary>
+        [Key]
         public int ID { get; set; }
         /// <summary>
         /// Identificador do Pedido.
         /// </summary>
+        [Required(ErrorMessage = "O campo Identificador é obrigatório.")]
+        [StringLength(255, ErrorMessage = "O campo Identificador deve ter no máximo 255 caracteres.")]
+        [Column(TypeName = "varchar")]
         public string Identificador { get; set; }
         /// <summary>
         /// Descrição do Pedido.
         /// </summary>
+        [StringLength(1000, ErrorMessage = "O campo Descrição deve ter no máximo 1000 caracteres.")]
+        [Column(TypeName = "varchar")]
         public string? Descricao { get; set; }
         /// <summary>
         /// Valor total do Pedido.
         /// </summary>
+        [Required(ErrorMessage = "O campo Valor Total é obrigatório.")]
+        [Column(TypeName = "decimal(21,2)")]
         public decimal ValorTotal { get; set; }
     }
 }
--- ConsoleAppPedidos/Models/Produto.cs
+++ ConsoleAppPedidos/Models/Produto.cs
@@ -1,21 +1,31 @@
-﻿namespace ConsoleAppPedidos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsoleAppPedidos.Models
 {
     /// <summary>
     /// Classe Produto.
     /// </summary>
+    [Table("Produtos")]
     public class Produto
     {
         /// <summary>
         /// ID do Produto.
         /// </summary>
+        [Key]
         public int ID { get; set; }
         /// <summary>
         /// Nome do Produto.
         /// </summary>
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(255, ErrorMessage = "O campo Nome deve ter no máximo 255 caracteres.")]
+        [Column(TypeName = "varchar")]
         public string Nome { get; set; }
         /// <summary>
         /// Categoria do Produto.
         /// </summary>
+        [Required(ErrorMessage = "O campo Categoria é obrigatório.")]
+        [Column(TypeName = "tinyint")]
         public int Categoria { get; set; }
     }
 }

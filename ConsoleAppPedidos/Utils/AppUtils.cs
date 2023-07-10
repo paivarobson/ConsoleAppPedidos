b9e8a4f73f826e29e92e87acb7dafde36c096085
Robson Paivaq <paivawork@hotmail.com>
2023-07-10 00:58:20 -0300

    Adicionado métodos no AppUtils.cs

--- ConsoleAppPedidos/Utils/AppUtils.cs
+++ ConsoleAppPedidos/Utils/AppUtils.cs
@@ -17,6 +17,24 @@
             else
                 return "1 - Não perecível";
         }
+
+        /// <summary>
+        /// Valida a resposta do usuário, verificando se é uma opção válida.
+        /// </summary>
+        /// <param name="respostaUsuario">A resposta do usuário a ser validada.</param>
+        /// <returns>True se a resposta for "y" ou "n", caso contrário retorna False.</returns>
+        public static bool ValidacaorespostaUsuario(string respostaUsuario)
+        {
+            return respostaUsuario.Equals("s") || respostaUsuario.Equals("n");
+        }
+
+        /// <summary>
+        /// Exibe uma mensagem informando que a opção fornecida pelo usuário é inválida.
+        /// </summary>
+        public static void MensagemRespostaInvalidaUsuario()
+        {
+            Console.WriteLine("Opção inválida. Digite s para SIM ou n para NÃO. Tente novamente.");
+        }
     }
 }
 

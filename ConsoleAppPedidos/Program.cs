79362faf23acbeeca2b89c8c12835c41bea1aa1d
Robson Paivaq <paivawork@hotmail.com>
2023-07-13 17:50:59 -0300

    Adicionada Injeção de Dependëncia

--- ConsoleAppPedidos/Program.cs
+++ ConsoleAppPedidos/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace ConsoleAppPedidos
+﻿using ConsoleAppPedidos.Data;
+using ConsoleAppPedidos.Data.Repositories;
+using ConsoleAppPedidos.Interfaces;
+using ConsoleAppPedidos.Interfaces.Data;
+using ConsoleAppPedidos.Interfaces.Services;
+using ConsoleAppPedidos.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleAppPedidos
 {
     /// <summary>
     /// Classe principal que contém o método de entrada do programa.
@@ -11,8 +19,24 @@
         /// <param name="args">Argumentos de linha de comando (opcional).</param>
         static void Main(string[] args)
         {
-            // Inicia a execução do programa exibindo o menu principal
-            MenuOpcoes.ExibirMenuPrincipal();
+            try
+            {
+                using (var serviceProvider = new ServiceCollection()
+                .AddSingleton<IAppDbContexto, AppDbContexto>()
+                .AddSingleton<IProdutoRepository, ProdutoRepository>()
+                .AddSingleton<IProdutoService, ProdutoService>()
+                .BuildServiceProvider())
+                {
+                    // Inicia a execução do programa exibindo o menu principal
+                    var menu = new MenuOpcoes(serviceProvider.GetRequiredService<IProdutoService>());
+
+                    menu.ExibirMenuPrincipal();
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }

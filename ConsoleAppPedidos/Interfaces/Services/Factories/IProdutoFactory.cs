a5a4d46ff8dfb839f17b3697daa9ecbff483b748
Robson Paivaq <paivawork@hotmail.com>
2023-07-14 23:31:42 -0300

    Adicionado fábrica de Produto. Padrão de projeto Factory.

---
+++ ConsoleAppPedidos/Interfaces/Services/Factories/IProdutoFactory.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleAppPedidos.Models;
+
+namespace ConsoleAppPedidos.Interfaces.Services.Factories
+{
+    /// <summary>
+    /// Interface responsável por definir um contrato para a criação de produtos.
+    /// </summary>
+    public interface IProdutoFactory
+    {
+        /// <summary>
+        /// Cria um novo produto com base na descrição e categoria fornecidas.
+        /// </summary>
+        /// <param name="descricao">A descrição do produto.</param>
+        /// <param name="categoria">A categoria do produto.</param>
+        /// <returns>O produto criado.</returns>
+        Produto CriarProduto(string descricao, int categoria);
+    }
+}
---
+++ ConsoleAppPedidos/Services/Factories/ProdutoFactory.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleAppPedidos.Interfaces.Services.Factories;
+using ConsoleAppPedidos.Models;
+
+namespace ConsoleAppPedidos.Services.Factories
+{
+    /// <summary>
+    /// Classe responsável por criar produtos.
+    /// </summary>
+    public class ProdutoFactory : IProdutoFactory
+    {
+        /// <summary>
+        /// Cria um novo produto com base na descrição e categoria fornecidas.
+        /// </summary>
+        /// <param name="descricao">A descrição do produto.</param>
+        /// <param name="categoria">A categoria do produto.</param>
+        /// <returns>O produto criado.</returns>
+        /// <exception cref="Exception">Exceção lançada quando ocorre um erro ao criar o produto.</exception>
+        public Produto CriarProduto(string descricao, int categoria)
+        {
+            try
+            {
+                var novoProduto = new Produto
+                {
+                    Descricao = descricao,
+                    Categoria = categoria
+                };
+
+                return novoProduto;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao criar o produto.", ex);
+            }
+        }
+    }
+}

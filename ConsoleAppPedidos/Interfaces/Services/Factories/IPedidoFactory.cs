26014d248121db41114940a887303178700021d8
Robson Paivaq <paivawork@hotmail.com>
2023-07-14 23:01:10 -0300

    Adicionado princípio de Agregados de DDD (Domain-Driven Design) ao Pedido

---
+++ ConsoleAppPedidos/Interfaces/Services/Factories/IPedidoFactory.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleAppPedidos.Models;
+
+namespace ConsoleAppPedidos.Interfaces.Services.Factories
+{
+    public interface IPedidoFactory
+    {
+        Pedido CriarPedido(string identificador, string descricao);
+        ItemDoPedido AdicionarItem(int produtoId, int quantidade, double valorUnitario);
+    }
+}
+
---
+++ ConsoleAppPedidos/Services/Factories/PedidoFactory.cs
@@ -0,0 +1,63 @@
+﻿using ConsoleAppPedidos.Interfaces.Services.Factories;
+using ConsoleAppPedidos.Models;
+
+namespace ConsoleAppPedidos.Services.Factories
+{
+    /// <summary>
+    /// Classe responsável por criar pedidos e itens de pedido.
+    /// </summary>
+    public class PedidoFactory : IPedidoFactory
+    {
+        /// <summary>
+        /// Cria um novo pedido com base no identificador e descrição fornecidos.
+        /// </summary>
+        /// <param name="identificador">O identificador do pedido.</param>
+        /// <param name="descricao">A descrição do pedido.</param>
+        /// <returns>O pedido criado.</returns>
+        /// <exception cref="Exception">Exceção lançada quando ocorre um erro ao criar o pedido.</exception>
+        public Pedido CriarPedido(string identificador, string descricao)
+        {
+            try
+            {
+                var novoPedido = new Pedido
+                {
+                    Identificador = identificador,
+                    Descricao = descricao
+                };
+
+                return novoPedido;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao criar o pedido", ex);
+            }
+        }
+
+        /// <summary>
+        /// Cria um novo item de pedido com base no produto ID, quantidade e valor unitário fornecidos.
+        /// </summary>
+        /// <param name="produtoId">O ID do produto.</param>
+        /// <param name="quantidade">A quantidade do item de pedido.</param>
+        /// <param name="valorUnitario">O valor unitário do item de pedido.</param>
+        /// <returns>O item de pedido criado.</returns>
+        /// <exception cref="Exception">Exceção lançada quando ocorre um erro ao adicionar o item ao pedido.</exception>
+        public ItemDoPedido AdicionarItem(int produtoId, int quantidade, double valorUnitario)
+        {
+            try
+            {
+                var novoItem = new ItemDoPedido
+                {
+                    ProdutoID = produtoId,
+                    Quantidade = quantidade,
+                    Valor = valorUnitario
+                };
+
+                return novoItem;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao adicionar item ao pedido", ex);
+            }
+        }
+    }
+}
